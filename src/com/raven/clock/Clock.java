7262433c5392d487af8bb648520413581587cbcd
laing raven <laingraven.java@gmail.com>
2021-10-11 22:30:58 +0700

    update

--- src/com/raven/clock/Clock.java
+++ src/com/raven/clock/Clock.java
@@ -43,6 +43,14 @@ public class Clock extends JComponent {
         thread.start();
     }
 
+    private void initTime() {
+        SimpleDateFormat df = new SimpleDateFormat("hh:mm:ss");
+        String t = df.format(new Date());
+        time.setHour(Integer.valueOf(t.split(":")[0]));
+        time.setMinute(Integer.valueOf(t.split(":")[1]));
+        time.setSeconds(Integer.valueOf(t.split(":")[2]));
+    }
+
     private void sleep() {
         try {
             Thread.sleep(1000);
@@ -51,12 +59,26 @@ public class Clock extends JComponent {
         }
     }
 
-    private void initTime() {
-        SimpleDateFormat df = new SimpleDateFormat("hh:mm:ss");
-        String t = df.format(new Date());
-        time.setHour(Integer.valueOf(t.split(":")[0]));
-        time.setMinute(Integer.valueOf(t.split(":")[1]));
-        time.setSeconds(Integer.valueOf(t.split(":")[2]));
+    @Override
+    public void paint(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int width = getWidth();
+        int height = getHeight();
+        int size = Math.min(width, height);
+        int x = (width - size) / 2;
+        int y = (height - size) / 2;
+        int centerX = width / 2;
+        int centerY = height / 2;
+        g2.drawImage(background, x, y, null);
+        drawMinute(g2, centerX, centerY, size / 2);
+        drawHour(g2, centerX, centerY, size / 2 - 14);
+        drawSeconds(g2, centerX, centerY, size / 2);
+        //  Create center point
+        int centerSize = 7;
+        g2.setColor(new Color(50, 50, 50));
+        g2.fillOval(centerX - centerSize / 2, centerY - centerSize / 2, centerSize, centerSize);
+        g2.dispose();
     }
 
     private void createBackground() {
@@ -74,25 +96,27 @@ public class Clock extends JComponent {
         g2.dispose();
     }
 
-    @Override
-    public void paint(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        int width = getWidth();
-        int height = getHeight();
-        int size = Math.min(width, height);
-        int x = (width - size) / 2;
-        int y = (height - size) / 2;
+    private void drawPoint(Graphics2D g2, int width, int height) {
         int centerX = width / 2;
         int centerY = height / 2;
-        int centerSize = 7;
-        g2.drawImage(background, x, y, null);
-        drawMinite(g2, centerX, centerY, size / 2);
-        drawHour(g2, centerX, centerY, size / 2 - 14);
-        drawSeconds(g2, centerX, centerY, size / 2);
-        g2.setColor(new Color(50, 50, 50));
-        g2.fillOval(centerX - centerSize / 2, centerY - centerSize / 2, centerSize, centerSize);
-        super.paint(grphcs);
+        float angle = 90;
+        int space = 360 / 12;
+        for (int i = 1; i <= 12; i++) {
+            int r = width / 2;
+            int s;
+            if (i % 3 == 0) {
+                g2.setColor(new Color(50, 50, 50));
+                g2.setStroke(new BasicStroke(2f));
+                s = 25;
+            } else {
+                g2.setColor(new Color(150, 150, 150));
+                g2.setStroke(new BasicStroke(1f));
+                s = 20;
+            }
+            Point locationStart = getLocationOf(angle - (space * i), r - s);
+            Point locationEnd = getLocationOf(angle - (space * i), r - 15);
+            g2.drawLine(centerX + locationStart.x, centerY - locationStart.y, centerX + locationEnd.x, centerY - locationEnd.y);
+        }
     }
 
     private void drawSeconds(Graphics2D g2, int centerX, int centerY, int size) {
@@ -107,7 +131,7 @@ public class Clock extends JComponent {
 
     private void drawHour(Graphics2D g, int centerX, int centerY, int size) {
         BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = (Graphics2D) img.createGraphics();
+        Graphics2D g2 = img.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         float angle = (360 / -12 * time.getHour()) + 90;
         double t = 30 * ((time.getMinute() * 100 / 60) / 100f);
@@ -120,7 +144,6 @@ public class Clock extends JComponent {
         Point pointRight = getLocationOf(angleRight, 7);
         Point pointCircle = getLocationOf(angleCircle, 15);
         g2.setColor(new Color(50, 50, 50));
-        g2.drawLine(centerX, centerY, centerX + point.x, centerY - point.y);
         Path2D.Float p = new Path2D.Float();
         p.moveTo(centerX + pointLeft.x, centerY - pointLeft.y);
         p.lineTo(centerX + point.x, centerY - point.y);
@@ -132,22 +155,22 @@ public class Clock extends JComponent {
         g2.fillOval(centerX - size / 2, centerY - size / 2, size, size);
         g.drawImage(new ShadowRenderer(5, 0.3f, Color.BLACK).createShadow(img), -4, -4, null);
         g.drawImage(img, 0, 0, null);
+        g2.dispose();
     }
 
-    private void drawMinite(Graphics2D g, int centerX, int centerY, int size) {
+    private void drawMinute(Graphics2D g, int centerX, int centerY, int size) {
         BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = (Graphics2D) img.createGraphics();
+        Graphics2D g2 = img.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         float angle = (360 / -60 * time.getMinute()) + 90;
         float angleLeft = angle + 90;
         float angleRight = angle - 90;
         float angleCircle = angle + 180;
-        Point point = getLocationOf(angle, size - 25);
+        Point point = getLocationOf(angle, size - 35);
         Point pointLeft = getLocationOf(angleLeft, 7);
         Point pointRight = getLocationOf(angleRight, 7);
         Point pointCircle = getLocationOf(angleCircle, 15);
         g2.setColor(new Color(255, 94, 94));
-        g2.drawLine(centerX, centerY, centerX + point.x, centerY - point.y);
         Path2D.Float p = new Path2D.Float();
         p.moveTo(centerX + pointLeft.x, centerY - pointLeft.y);
         p.lineTo(centerX + point.x, centerY - point.y);
@@ -159,29 +182,7 @@ public class Clock extends JComponent {
         g2.fillOval(centerX - size / 2, centerY - size / 2, size, size);
         g.drawImage(new ShadowRenderer(5, 0.3f, Color.BLACK).createShadow(img), -4, -4, null);
         g.drawImage(img, 0, 0, null);
-    }
-
-    private void drawPoint(Graphics2D g2, int width, int height) {
-        int centerX = width / 2;
-        int centerY = height / 2;
-        float angle = 90;
-        int space = 360 / 12;
-        for (int i = 1; i <= 12; i++) {
-            int r = width / 2;
-            int s;
-            if (i % 3 == 0) {
-                g2.setColor(new Color(50, 50, 50));
-                g2.setStroke(new BasicStroke(2f));
-                s = 25;
-            } else {
-                g2.setStroke(new BasicStroke(1f));
-                g2.setColor(new Color(150, 150, 150));
-                s = 20;
-            }
-            Point locationStart = getLocationOf(angle - (space * i), r - s);
-            Point locationEnd = getLocationOf(angle - (space * i), r - 15);
-            g2.drawLine(centerX + locationStart.x, centerY - locationStart.y, centerX + locationEnd.x, centerY - locationEnd.y);
-        }
+        g2.dispose();
     }
 
     private BufferedImage createCircle(int size, int shadowSize) {
@@ -192,6 +193,7 @@ public class Clock extends JComponent {
         g2.fillOval(shadowSize, shadowSize, size - shadowSize * 2, size - shadowSize * 2);
         g2.drawImage(new ShadowRenderer(shadowSize, 0.3f, new Color(50, 50, 50)).createShadow(img), -shadowSize, -shadowSize, null);
         g2.fillOval(shadowSize, shadowSize, size - shadowSize * 2, size - shadowSize * 2);
+        g2.dispose();
         return img;
     }
 
